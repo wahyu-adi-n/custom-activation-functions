255e3c6dfa7773ac8dbb46b2ba912396ef0db0f6
Wahyu Adi Nugroho <66587578+wahyu-adi-n@users.noreply.github.com>
2023-11-29 00:28:27 +0000

    fix minor

--- train.py
+++ train.py
@@ -153,5 +153,3 @@ with open("assets/logs/densenet_121_results.csv", mode="w") as csv_file:
             plt.savefig(f"assets/loss_plots/{text}_DenseNet_121_Model_Loss_{i}.png")
             plt.figure()
             plt.clf()
-
-            plt.close()
