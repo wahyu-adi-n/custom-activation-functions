479602ba707700805e5a339b3681f14550a518dd
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2023-12-02 04:02:08 +0000

    add clahe function

---
+++ utils/clahe.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+import numpy as np
+
+root_path = "chest_xray/"
+
+for dirpath, dirnames, filenames in os.walk(root_path):
+    for file in filenames:
+        image_path = os.path.join(dirpath, file)
+        
+        image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+        
+        # Enhance contrast
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        enhanced_image = clahe.apply(image)
+        
+
+        # Adjust brightness
+        alpha = 1.2  # Brightness factor (1.0 is neutral)
+        adjusted_image = np.clip(alpha * enhanced_image, 0, 255).astype(np.uint8)
+        cv2.imwrite(image_path, adjusted_image)
+        
+        
